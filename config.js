1aa8b8d128bdd5bd57612dc27e89fcb1494311bf
Sandunshaki <156651218+Sandunshaki@users.noreply.github.com>
2024-01-15 13:50:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,10 +8,10 @@ import axios from 'axios'
 
 global.owner = [
   ['918360234087', 'WHITE', true], 
-  ['233208179431', 'Abraham Dwamena', true],
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
-  ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
-  ['233533763772', 'Abraham Dwamena2', true],
+  ['94766910147', 'MR|SADU', true],
+  ['94757101708', 'MR|SADU', true], 
+  ['', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
+  ['', 'Abraham Dwamena2', true],
   
 ] //Number of owners
 
@@ -54,10 +54,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'XLICON-V2'
+global.botname = 'KING INZANE'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
-global.author = '@XLICON-V2' 
+global.packname = 'INZANE TEAM' 
+global.author = '@MR|SADU' 
 global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
 global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
 global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
